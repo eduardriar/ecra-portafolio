c37c5acb436bf593da3595c148cdb4706b580b65
Eduard <eduardriar@unisabana.edu.co>
2023-01-09 19:55:56 -0500

    AboutSection page added and some other fixtures

--- src/Pages/Sections/AboutMeSection/AboutMeSection.jsx
+++ src/Pages/Sections/AboutMeSection/AboutMeSection.jsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect, useRef } from 'react';
+import '../AboutMeSection/AboutMeSection.scss'
 
 const AboutMeSection = () => {
 
     return (
         <section className='about-me-section'>
-            <div>
-                
-            </div>
+            <h1>My name is Eduard, and I'm a Web Developer</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore rem ipsam dolore? Iusto, illo nulla, fuga minima perspiciatis sint quaerat, in ipsa eum sequi modi natus? Totam odit impedit ea.</p>
+            <div className='about-me-section-image'></div>
         </section>
     )
 }
